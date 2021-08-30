3313cea77298dbae920d9544b8619b5438aa4477
Rob Kellen <rob.h.kellen@gmail.com>
2021-08-30 16:33:31 -0700

    edited FeaturedProducts component to be more responsive to smaller screen sizes.  Resolved issue with PromotionalProducts component that was causing overflow on x axis because of margins for secondary images in carousel.

--- frontend/src/components/home/MarketingButtons.js
+++ frontend/src/components/home/MarketingButtons.js
@@ -22,9 +22,33 @@ const useStyles = makeStyles(theme => ({
     "&:hover": {
       transform: "scale(1.1)",
     },
+    [theme.breakpoints.down("lg")]: {
+      height: "40rem",
+      width: "40rem",
+      margin: "3rem",
+    },
+    [theme.breakpoints.down("sm")]: {
+      height: "30rem",
+      width: "30rem",
+    },
+    [theme.breakpoints.down("xs")]: {
+      height: "25rem",
+      width: "25rem",
+    },
   },
   container: {
     margin: "15rem 0",
+  },
+  icon: {
+    [theme.breakpoints.down("sm")]: {
+      height: "8rem",
+      width: "8rem",
+    },
+  },
+  label: {
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "2.75rem",
+    },
   },
 }))
 
