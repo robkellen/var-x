56010ce4484c3ee559a460265b310f06ad51fc98
Rob Kellen <rob.h.kellen@gmail.com>
2021-09-01 11:27:49 -0700

    created validate function in ui directory to use for validation of inputs in contact.js page.  imported it in contact.js

---
+++ frontend/src/components/ui/validate.js
@@ -0,0 +1,19 @@
+export default function validate(values) {
+  // input: values = {field: value, field2: value} eg {email: rob.h.kellen@gmail.com, phone: 555-555-5555}
+  const validators = {
+    email: val => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(val),
+    phone: val =>
+      /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(val),
+    name: val => val.length > 3,
+    message: val => val.length > 3,
+  }
+
+  // output: {field: valid} eg {email: true, phone: true}
+
+  const valid = {}
+
+  Object.keys(values).map(field => {
+    valid[field] = validators[field](values[field])
+  })
+  return valid
+}
