afcff1878e53c9dfdcd145f8d89a9316acd76480
Rob Kellen <rob.h.kellen@gmail.com>
2021-09-03 14:15:22 -0700

    created Sort comoponent to show Chips that will allow user to sort the product list in various ways.  Added functionality to close button to close the available sort options and return to main header options.

---
+++ frontend/src/components/product-list/FunctionContainer.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react"
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import IconButton from "@material-ui/core/IconButton"
+import { makeStyles } from "@material-ui/core/styles"
+
+import Sort from "./Sort"
+
+//images
+import filter from "../../images/filter.svg"
+import sort from "../../images/sort.svg"
+
+const useStyles = makeStyles(theme => ({
+  functionContainer: {
+    backgroundColor: theme.palette.primary.main,
+    height: "6rem",
+    borderRadius: "10px 10px 0px 0px",
+  },
+}))
+
+export default function FunctionContainer() {
+  const classes = useStyles()
+
+  //set state for sort/filter functionality
+  const [option, setOption] = useState(null)
+
+  const content = () => {
+    switch (option) {
+      case null:
+        const items = [
+          { icon: filter, alt: "filter" },
+          { icon: sort, alt: "sort" },
+        ]
+        return (
+          <Grid
+            item
+            container
+            justifyContent="space-around"
+            alignItems="center"
+          >
+            {items.map(item => (
+              <Grid item key={item.alt}>
+                <IconButton onClick={() => setOption(item.alt)}>
+                  <img src={item.icon} alt={item.alt} />
+                </IconButton>
+              </Grid>
+            ))}
+          </Grid>
+        )
+      case "sort":
+        return <Sort setOption={setOption} />
+      default:
+        return null
+    }
+  }
+
+  return (
+    <Grid item container classes={{ root: classes.functionContainer }}>
+      {content()}
+    </Grid>
+  )
+}
