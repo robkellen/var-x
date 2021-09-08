b18497526898541dffc7573c537e3322e05b405a
Rob Kellen <rob.h.kellen@gmail.com>
2021-09-08 12:53:46 -0700

    added image and name of each product to ProductGridFrame and styled them with spacing between each listed product variant.

--- frontend/src/components/product-list/ProductFrameGrid.js
+++ frontend/src/components/product-list/ProductFrameGrid.js
@@ -15,16 +15,43 @@ const useStyles = makeStyles(theme => ({
     backgroundRepeat: "no-repeat",
     height: "25rem",
     width: "25rem",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  product: {
+    height: "20rem",
+    width: "20rem",
+  },
+  title: {
+    backgroundColor: theme.palette.primary.main,
+    height: "5rem",
+    width: "25rem",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "-0.1rem",
   },
 }))
 
-export default function ProductFrameGrid({ product }) {
+export default function ProductFrameGrid({ product, variant }) {
   const classes = useStyles()
 
   return (
     <Grid item>
       <Grid container direction="column">
-        <Grid item classes={{ root: classes.frame }}></Grid>
+        <Grid item classes={{ root: classes.frame }}>
+          <img
+            src={process.env.GATSBY_STRAPI_URL + variant.images[0].url}
+            alt={product.node.name}
+            className={classes.product}
+          />
+        </Grid>
+        <Grid item classes={{ root: classes.title }}>
+          <Typography variant="h5">
+            {product.node.name.split(" ")[0]}
+          </Typography>
+        </Grid>
       </Grid>
     </Grid>
   )
