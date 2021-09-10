bd851af1495bdacaa2d7c63a18c6cec996586323
Rob Kellen <rob.h.kellen@gmail.com>
2021-09-09 21:25:05 -0700

    created Fab button in ProductList component to allow user to click and scroll back up to the top of the page starting at the top of the DynamicToolbar

--- frontend/src/templates/ProductList.js
+++ frontend/src/templates/ProductList.js
@@ -1,10 +1,25 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import Grid from "@material-ui/core/Grid"
+import Fab from "@material-ui/core/Fab"
 import { graphql } from "gatsby"
+import { makeStyles } from "@material-ui/core/styles"
 
 import Layout from "../components/ui/layout"
 import DynamicToolbar from "../components/product-list/DynamicToolbar"
 import ListOfProducts from "../components/product-list/ListOfProducts"
+
+const useStyles = makeStyles(theme => ({
+  fab: {
+    alignSelf: "flex-end",
+    marginRight: "2rem",
+    marginBottom: "2rem",
+    color: "#fff",
+    fontFamily: "Montserrat",
+    fontSize: "5rem",
+    width: "5rem",
+    height: "5rem",
+  },
+}))
 
 export default function ProductList({
   pageContext: { filterOptions, name, description },
@@ -12,12 +27,24 @@ export default function ProductList({
     allStrapiProduct: { edges: products },
   },
 }) {
+  const classes = useStyles()
+
   // set initial state for background color of selected button
   const [layout, setLayout] = useState("grid")
+
+  // determine where on page user is to implement functionality to scroll to top Fab
+  const scrollRef = useRef(null)
+
+  // access where user is and scroll back up to top of toolbar when Fab is clicked
+  const scroll = () => {
+    scrollRef.current.scrollIntoView({ behavior: "smooth" })
+  }
 
   return (
     <Layout>
       <Grid container direction="column" alignItems="center">
+        {/* div to use as reference for Fab button to scroll to top of DynamicToolbar */}
+        <div ref={scrollRef} />
         <DynamicToolbar
           filterOptions={filterOptions}
           name={name}
@@ -26,6 +53,9 @@ export default function ProductList({
           setLayout={setLayout}
         />
         <ListOfProducts products={products} layout={layout} />
+        <Fab onClick={scroll} color="primary" classes={{ root: classes.fab }}>
+          ^
+        </Fab>
       </Grid>
     </Layout>
   )
