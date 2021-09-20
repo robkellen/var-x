b616de9469c9d97bdfec87bf822a4cad5cfb8688
Rob Kellen <rob.h.kellen@gmail.com>
2021-09-20 14:01:23 -0700

    set up ProductDetail page to add products that user views into localStorage to be retrieved later and displayed in RecentlyViewed component.

--- frontend/src/templates/ProductDetail.js
+++ frontend/src/templates/ProductDetail.js
@@ -6,7 +6,7 @@ import ProductImages from "../components/product-detail/ProductImages"
 import ProductInfo from "../components/product-detail/ProductInfo"
 
 export default function ProductDetail({
-  pageContext: { name, id, category, description, variants },
+  pageContext: { name, id, category, description, variants, product },
 }) {
   // set initial state of which variant/image of the product we want
   const [selectedVariant, setSelectedVariant] = useState(0)
@@ -18,6 +18,28 @@ export default function ProductDetail({
     const styledVariant = variants.filter(
       variant => variant.style === params.get("style")
     )[0]
+
+    // get recently viewed items from local storage
+    let recentlyViewed = JSON.parse(
+      window.localStorage.getItem("recentlyViewed")
+    )
+    if (recentlyViewed) {
+      // if 10 products exist in recentlyViewed array remove the oldest product and push the new product in
+      if (recentlyViewed.length === 10) {
+        recentlyViewed.shift()
+      }
+      if (!recentlyViewed.some(product => product.node.name === name)) {
+        recentlyViewed.push(product)
+      }
+    } else {
+      recentlyViewed = [product]
+    }
+
+    window.localStorage.setItem(
+      "recentlyViewed",
+      JSON.stringify(recentlyViewed)
+    )
+
     setSelectedVariant(variants.indexOf(styledVariant))
   }, [])
 
