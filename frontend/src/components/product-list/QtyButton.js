1e95233d5a64deb9ee634af863be1f4000622cec
Rob Kellen <rob.h.kellen@gmail.com>
2021-10-14 12:31:40 -0700

    edited QtyButton styles if being displayed on cart page.  added functionality to +/- buttons to update actual quantity of items in cart if user changes the quantity

--- frontend/src/components/product-list/QtyButton.js
+++ frontend/src/components/product-list/QtyButton.js
@@ -8,14 +8,14 @@ import Badge from "@material-ui/core/Badge"
 import { makeStyles } from "@material-ui/core/styles"
 
 import { CartContext } from "../../contexts"
-import { addToCart } from "../../contexts/actions"
+import { addToCart, removeFromCart } from "../../contexts/actions"
 
 //images
 import Cart from "../../images/Cart"
 
 const useStyles = makeStyles(theme => ({
   qtyText: {
-    color: "#fff",
+    color: ({ isCart }) => (isCart ? theme.palette.secondary.main : "#fff"),
   },
   mainGroup: {
     height: "3rem",
@@ -23,15 +23,18 @@ const useStyles = makeStyles(theme => ({
   editButtons: {
     height: "1.525rem",
     borderRadius: 0,
-    backgroundColor: theme.palette.secondary.main,
-    borderLeft: "2px solid #fff",
-    borderRight: "2px solid #fff",
+    backgroundColor: ({ isCart }) =>
+      isCart ? "#fff" : theme.palette.secondary.main,
+    borderLeft: ({ isCart }) =>
+      `2px solid ${isCart ? theme.palette.secondary.main : "#fff"} `,
+    borderRight: "2px solid  #fff",
     borderBottom: "none",
     borderTop: "none",
   },
   endButtons: {
     borderRadius: 50,
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: ({ isCart }) =>
+      isCart ? "#fff" : theme.palette.secondary.main,
     border: "none",
   },
   cartButton: {
@@ -39,14 +42,16 @@ const useStyles = makeStyles(theme => ({
     transition: "background-color 1s ease",
   },
   minusButton: {
-    borderTop: "2px solid #fff",
+    borderTop: ({ isCart }) =>
+      `2px solid ${isCart ? theme.palette.secondary.main : "#fff"} `,
   },
   minus: {
     marginTop: "-0.25rem",
   },
   qtyButton: {
     "&:hover": {
-      backgroundColor: theme.palette.secondary.main,
+      backgroundColor: ({ isCart }) =>
+        isCart ? "#fff" : theme.palette.secondary.main,
     },
   },
   badge: {
@@ -63,17 +68,28 @@ const useStyles = makeStyles(theme => ({
   },
 }))
 
-export default function QtyButton({ stock, variants, selectedVariant, name }) {
-  const classes = useStyles()
-
-  // set initial state to hold quantity of items
-  const [qty, setQty] = useState(1)
-
-  // state to select what to display for button
-  const [success, setSuccess] = useState(false)
+export default function QtyButton({
+  stock,
+  variants,
+  selectedVariant,
+  name,
+  isCart,
+}) {
+  const classes = useStyles({ isCart })
 
   // reducer to add items to the cart
   const { cart, dispatchCart } = useContext(CartContext)
+
+  // check to see if cart has items in it before setting initital state of qty button
+  const existingItem = cart.find(
+    item => item.variant === variants[selectedVariant]
+  )
+
+  // set initial state to hold quantity of items
+  const [qty, setQty] = useState(isCart ? existingItem.qty : 1)
+
+  // state to select what to display for button
+  const [success, setSuccess] = useState(false)
 
   // handler to determine quantity of items to add/subtract in cart
   const handleChange = direction => {
@@ -88,13 +104,22 @@ export default function QtyButton({ stock, variants, selectedVariant, name }) {
     }
 
     const newQty = direction === "up" ? qty + 1 : qty - 1
+
     setQty(newQty)
+
+    if (isCart) {
+      if (direction === "up") {
+        dispatchCart(addToCart(variants[selectedVariant], 1, name))
+      } else if (direction === "down") {
+        dispatchCart(removeFromCart(variants[selectedVariant], 1))
+      }
+    }
   }
 
   // functionality to add items to the cart
   const handleCart = () => {
     setSuccess(true)
-    
+
     dispatchCart(
       addToCart(
         variants[selectedVariant],
@@ -155,28 +180,30 @@ export default function QtyButton({ stock, variants, selectedVariant, name }) {
             </Typography>
           </Button>
         </ButtonGroup>
-        <Button
-          classes={{
-            root: clsx(classes.endButtons, classes.cartButton, {
-              [classes.success]: success,
-            }),
-          }}
-          onClick={handleCart}
-        >
-          {success ? (
-            <Typography variant="h3" classes={{ root: classes.qtyText }}>
-              ✓
-            </Typography>
-          ) : (
-            <Badge
-              overlap="circular"
-              badgeContent="+"
-              classes={{ badge: classes.badge }}
-            >
-              <Cart color="#fff" />
-            </Badge>
-          )}
-        </Button>
+        {isCart ? null : (
+          <Button
+            classes={{
+              root: clsx(classes.endButtons, classes.cartButton, {
+                [classes.success]: success,
+              }),
+            }}
+            onClick={handleCart}
+          >
+            {success ? (
+              <Typography variant="h3" classes={{ root: classes.qtyText }}>
+                ✓
+              </Typography>
+            ) : (
+              <Badge
+                overlap="circular"
+                badgeContent="+"
+                classes={{ badge: classes.badge }}
+              >
+                <Cart color="#fff" />
+              </Badge>
+            )}
+          </Button>
+        )}
       </ButtonGroup>
     </Grid>
   )
