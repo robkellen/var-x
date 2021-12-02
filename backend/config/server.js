a8b8cca85122a679940a5ac2460e30b98d19f299
Rob Kellen <rob.h.kellen@gmail.com>
2021-12-02 21:26:47 +0000

    updated server config

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   cron: { enabled: true },
+  url: env("URL", "http://localhost"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "a51cf181326f38cb89eff55bec4f6474"),
